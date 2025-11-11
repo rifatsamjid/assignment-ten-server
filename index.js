062ea466a22ac4a5a7cc6ccf6349b563371bf4bb
rifatsamjid <rifattamim74328@gmail.com>
2025-11-11 20:02:02 +0600

    creat a server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 4000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.send("assignment ten server is running")
+})
+
+app.listen(port,()=>{
+    console.log(`Assignment ten running port:${port}`)
+})
