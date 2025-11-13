f3d1e4ad7f83a1598f2efa56e99e5a148caab7c9
rifatsamjid <rifattamim74328@gmail.com>
2025-11-13 16:07:04 +0600

    creat a function for users send server

--- index.js
+++ index.js
@@ -29,6 +29,22 @@ async function run() {
 
         const db = client.db("movie_db")
         const moviesCollection = db.collection("movies")
+         const usersCollection = db.collection('users');
+
+        // users api
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const email = req.body.email
+            const query = { email: email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                res.send({ notification: 'user already exits .do not need to insert again' })
+            }
+            else {
+                const result = await usersCollection.insertOne(newUser)
+                res.send(result)
+            }
+        })
 
 
         // api add
